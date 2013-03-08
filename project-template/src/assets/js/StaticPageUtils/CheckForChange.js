372a10c1b4b78ff257c8f87c46dbd4e8feaf406f
joseph <joseph@JHP>
2013-03-08 00:33:55 -0700

    Remembering scroll position.

--- project-template/src/assets/js/StaticPageUtils/CheckForChange.js
+++ project-template/src/assets/js/StaticPageUtils/CheckForChange.js
@@ -1,16 +1,28 @@
 !function(){
-var script;
-var body;
-setTimeout(function(){
-   body=document.body;
-   insertScript();
-}, 5000);
-setInterval(function(){
-   if(body){
-      body.removeChild(script);
+   var s=sessionStorage;
+   var x='CheckForChange_SX';
+   var y='CheckForChange_SY';
+   var script;
+   var body;
+
+   setTimeout(function(){
+      if(s[x]){
+         window.scrollTo(~~s[x],~~s[y]);
+      }
+   }, 500);
+   setTimeout(function(){
+      body=document.body;
       insertScript();
-   }
-}, 2000);
+
+   }, 5000);
+   setInterval(function(){
+      if(body){
+         body.removeChild(script);
+         s[x]=window.scrollX;
+         s[y]=window.scrollY;
+         insertScript();
+      }
+   }, 2000);
    function insertScript(){
       script = document.createElement('script');
       script.src="refresh.js?"+Date.now();
